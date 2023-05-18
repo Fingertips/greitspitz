1d1a57224402e5c868890646aab91210d1fcb0ab
Manfred Stienstra <manfred@fngtps.com>
2023-05-18 11:29:40 +0200

    Configure Log and start server when running binary.

--- spec/cases/cli_spec.cr
+++ spec/cases/cli_spec.cr
@@ -1,8 +1,9 @@
 require "../spec_helper"
 
+# NOTE: There are functional tests for the server because a lot of this is glue code that
+# eventually starts an infinitely waiting HTTP server.
 describe Greitspitz::Cli do
-  it "runs" do
-    cli = Greitspitz::Cli.new
-    cli.run([""])
+  it "initializes" do
+    Greitspitz::Cli.new
   end
 end
--- src/greitspitz/cli.cr
+++ src/greitspitz/cli.cr
@@ -1,4 +1,5 @@
 module Greitspitz
+  # Configures the environment and starts the server or other commands.
   class Cli
     def initialize
       @context = Context.new
@@ -6,6 +7,24 @@ module Greitspitz
 
     def run(argv)
       OptionParser.parse(argv, @context)
+      run_command if @context.run?
+    end
+
+    private def run_command
+      setup_logging
+      start_http_server
+    end
+
+    private def setup_logging
+      if @context.log_level
+        ::Log.setup(@context.log_level.not_nil!)
+      else
+        ::Log.setup_from_env
+      end
+    end
+
+    private def start_http_server
+      HttpServer.new(@context).listen
     end
   end
 end
